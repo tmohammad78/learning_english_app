c25f3bf156a10eea5ea136940c7374e0199695c4
tmohammad78 <mohammadtaheri506@gmail.com>
2021-08-31 23:03:28 +0430

    Add config

---
+++ src/components/pages/home/index.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import App from '../../../App';
+describe('<LoginForm />', () => {
+  test('should display a blank login form, with remember me checked by default', async () => {
+    const a = 4;
+    expect(a).toEqual(4);
+    render(<App />);
+    expect(screen.getByText('Learn React')).toBeInTheDocument();
+  });
+});
