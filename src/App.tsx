ab65a026c6843afd417bed947bb2b13a1c6d57a4
tmohammad78 <mohammadtaheri506@gmail.com>
2021-09-01 21:37:19 +0430

    Add button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-import logo from './logo.svg';
-import Home from 'src/components/pages/home';
 import './App.scss';
-import Button from 'src/components/button';
 
 function App() {
-  function click() {
-    console.log('this is test');
-  }
-  return (
-    <div className="App">
-      <Button handleClick={click} customClass="customButton">
-        <img src={logo} className="logo" />
-      </Button>
-    </div>
-  );
+  return <div className="App">Learn React</div>;
 }
 
 export default App;
